97f4cb1ae9707b69a5d8fd2227cdc4fafde03802
NgallaJuke <ngalladiouck@gmail.com>
2019-10-27 23:57:24 +0000

    Sending message from the form

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,18 @@
 // DOM queries
 const chatList = document.querySelector('.chat-list');
 
+//Add a new chat
+const newChatForm = document.querySelector('.new-chat');
+
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => {
+      newChatForm.reset();
+    })
+    .catch(error => console.log(error));
+});
 // Class instance
 const chatUi = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'Ngalla');

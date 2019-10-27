b3acc14cb42c166ac2a331de87943f7e742c48f8
NgallaJuke <ngalladiouck@gmail.com>
2019-10-27 23:45:30 +0000

    Formating the date

--- Scripts/ui.js
+++ Scripts/ui.js
@@ -10,11 +10,13 @@ class ChatUI {
   }
 
   render(data) {
+
+    const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), { addSuffix: true });
     const html = `
       <li class="list-group-item">
       <span class="username">${data.username}</span>
       <span class=""message>${data.message}</span>
-      <div>${data.created_at.toDate()}</div>
+      <div class="time">${when}</div>
       </li>
     `;
     this.list.innerHTML += html;
